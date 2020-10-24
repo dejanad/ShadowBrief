954dab2256738306430ba2950cbf13bf08b7639b
dejanad <55702240+dejanad@users.noreply.github.com>
2020-10-24 19:49:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,3 +13,19 @@ function draw() {
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
+
+//collapsible text
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
